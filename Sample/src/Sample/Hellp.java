4807ea59998b3e647933c8471bf17969b7a24ba2
Pavan R Shetty <pavanrshetty3@gmail.com>
2021-02-25 20:41:58 +0530

    Updated

---
+++ Sample/src/Sample/Hellp.java
@@ -0,0 +1,11 @@
+package Sample;
+
+public class Hellp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello ");
+		System.out.println("Hi");
+	}
+
+}
