a50be1c8ad141690a76c1c4790578e6b86027477
PavanShetty3 <64677732+PavanShetty3@users.noreply.github.com>
2021-02-25 12:19:39 +0530

    Add files via upload

---
+++ FileCopy.java
@@ -0,0 +1,29 @@
+package Day8;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileCopy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		File file=new File("D:\\Vowels and Consonents");
+		try {
+			FileInputStream in=new FileInputStream(file+"\\Count");
+			FileOutputStream out=new FileOutputStream(file+"\\Yes");
+			int c;
+			while((c=in.read())!=-1) {
+				out.write(c);
+			}
+			in.close();
+			out.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Check");
+	}
+}
